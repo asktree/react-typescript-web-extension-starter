9fcbc67250d09dc8c18171ae2030a1d57bc20f0a
asktree <agrippakellum@gmail.com>
2021-02-28 15:31:06 -0600

    making a localstorage API

---
+++ src/data/client.ts
@@ -0,0 +1,52 @@
+import { Tabs, browser } from "webextension-polyfill-ts";
+import { uid } from "uid";
+import { add } from "date-fns";
+import { handle } from "@src/util";
+import { flow } from "fp-ts/function";
+import * as TE from "fp-ts/lib/TaskEither";
+import * as T from "fp-ts/lib/Task";
+
+export type SchedulerInstructions =
+  | {
+      showBy: Date;
+    }
+  | "none";
+
+export type History = [Date, any][];
+
+interface TabSnapshot extends Tabs.Tab {
+  scrollPosition: [vertical: number, horizontal: number];
+}
+
+export namespace Item {
+  export type t = {
+    id: string;
+    tabs: TabSnapshot[];
+    createdAt: Date;
+    history: History;
+    schedulerInstructions: SchedulerInstructions;
+  };
+  export const make = (tabs: TabSnapshot[]): t => ({
+    id: uid(),
+    tabs,
+    createdAt: new Date(),
+    history: [],
+    schedulerInstructions: "none",
+  });
+}
+
+export namespace UserData {
+  export type t = {
+    items: Record<string, Item.t>;
+  };
+  export const get = () => T.of(browser.storage.sync.get() as Promise<t>);
+  export const set = (draft: t) => T.of(browser.storage.sync.set(draft));
+
+  export const insertItem = async (item: Item.t) => {
+    const draft = await get()();
+    draft.items[item.id] = item;
+    await set(draft)();
+  };
+
+  export const createItem = flow(Item.make, insertItem);
+}
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,12 @@
 import { browser } from "webextension-polyfill-ts";
 
-export const log = (...args: any[]) => (browser.extension.getBackgroundPage() as unknown as {console: Console}).console.log(...args);
+export const log = (...args: any[]) =>
+  ((browser.extension.getBackgroundPage() as unknown) as {
+    console: Console;
+  }).console.log(...args);
+
+export const handle = <T extends unknown>(promise: Promise<T>) => {
+  return promise
+    .then((data) => [data, undefined] as const)
+    .catch((error) => Promise.resolve([undefined, error] as const));
+};
